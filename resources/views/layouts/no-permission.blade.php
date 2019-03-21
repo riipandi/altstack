2c19573cc23155ef0477f891086dd2ffdf12513c
Tim Ruhay <timruhay@gmail.com>
2019-03-21 23:05:00 +0700

    Little change permission view

---
+++ resources/views/layouts/no-permission.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+    <div class="w-full p-6">
+        <p>You don't have permission to access this page!</p>
+    </div>
+</div>
--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -3,15 +3,14 @@
 @section('content')
     <div class="flex items-center mb-6">
         <div class="container mx-auto">
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-                <div class="w-full p-6">
-                    @role('superadmin')
-                        I am a superadmin, yeaaayy!
-                    @else
-                        You don't have permission to access this page!
-                    @endrole
+            @role('superadmin')
+                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+                    <div class="w-full p-6">
+                        <p>I am a superadmin, yeaaayy!</p>
+                    </div>
                 </div>
-            </div>
+            @else @include('layouts.no-permission')
+            @endrole
         </div>
     </div>
 @endsection
