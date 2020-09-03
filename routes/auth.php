4c0331e7f795ea1d10fd49401bb4f1607e90821f
Aris Ripandi <ar.is@outlook.com>
2020-09-03 12:43:56 +0700

    Change auth routes (use grouping)

--- routes/auth.php
+++ routes/auth.php
@@ -8,37 +8,40 @@
 
 Route::redirect('/', '/auth/login', 302)->name('auth');
 
-// Login routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login')->name('login');
+Route::namespace('Auth')->group(function () {
 
-// Logout routes...
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    // Login routes...
+    Route::get('login', 'LoginController@showLoginForm')->name('login');
+    Route::post('login', 'LoginController@login')->name('login');
 
-// Registration routes...
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register')->name('register');
+    // Logout routes...
+    Route::get('logout', 'LoginController@logout')->name('logout');
+    Route::post('logout', 'LoginController@logout')->name('logout');
 
-// Email verification...
-Route::get('verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::post('resend-verification', 'Auth\VerificationController@resend')->name('verification.resend');
+    // Registration routes...
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'RegisterController@register')->name('register');
 
-// Reset user password...
-Route::get('resetpass', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::get('resetpass/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('resetpass', 'Auth\ResetPasswordController@reset')->name('password.update');
+    // Email verification...
+    Route::get('verify', 'VerificationController@show')->name('verification.notice');
+    Route::get('verify/{id}/{hash}', 'VerificationController@verify')->name('verification.verify');
+    Route::post('resend-verification', 'VerificationController@resend')->name('verification.resend');
 
-// Password confirmation...
-Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
-Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm')->name('password.confirm');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    // Reset user password...
+    Route::get('resetpass', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::get('resetpass/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('resetpass', 'ResetPasswordController@reset')->name('password.update');
 
-// Two factor authentication...
-// Route::get('twofactor', 'Auth\TwoFactorController@index')->name('login.twofactor');
-// Route::post('twofactor', 'Auth\TwoFactorController@verify')->name('login.twofactor.verify');
+    // Password confirmation...
+    Route::get('password/confirm', 'ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+    Route::post('password/confirm', 'ConfirmPasswordController@confirm')->name('password.confirm');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
-// Socialite route must be placed in the bottom.
-// Route::get('{provider}', 'Auth\SocialController@redirectToProvider')->name('auth.social');
-// Route::get('{provider}/callback', 'Auth\SocialController@handleProviderCallback')->name('auth.social.callback');
+    // Two factor authentication...
+    // Route::get('twofactor', 'TwoFactorController@index')->name('login.twofactor');
+    // Route::post('twofactor', 'TwoFactorController@verify')->name('login.twofactor.verify');
+
+    // Socialite route must be placed in the bottom.
+    // Route::get('{provider}', 'SocialController@redirectToProvider')->name('auth.social');
+    // Route::get('{provider}/callback', 'SocialController@handleProviderCallback')->name('auth.social.callback');
+});
