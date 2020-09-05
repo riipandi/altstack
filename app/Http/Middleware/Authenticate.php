929164b25bb23575dcae46299c20a78312b9bcc3
Aris Ripandi <ar.is@outlook.com>
2020-09-06 03:13:58 +0700

    Add query string on login url when unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\URL;
 
 class Authenticate extends Middleware
 {
@@ -15,8 +17,32 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (!$request->expectsJson()) {
-            return route('login');
+        if (! $request->expectsJson()) {
+            return self::qs_url('login', ['next' => Request::fullUrl()]);
         }
+    }
+
+    /**
+     * Generate a querystring url for the application.
+     *
+     * Assumes that you want a URL with a querystring rather than route params
+     * (which is what the default url() helper does)
+     *
+     * @param  string  $path
+     * @param  mixed   $qs
+     * @param  bool    $secure
+     * @return string
+     */
+    private static function qs_url($path = null, $qs = [], $secure = null)
+    {
+        $url = app('url')->to(route($path), $secure);
+        if (count($qs)) {
+            foreach ($qs as $key => $value) {
+                $qs[$key] = sprintf('%s=%s', $key, urlencode($value));
+            }
+            $url = sprintf('%s?%s', $url, implode('&', $qs));
+        }
+
+        return $url;
     }
 }
