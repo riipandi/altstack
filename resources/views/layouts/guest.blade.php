17d829f79255e8597574e6c5fd409f73cc85a862
Aris Ripandi <ar.is@outlook.com>
2020-09-13 05:05:15 +0700

    Add logout with get method

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,12 @@
 
 @section('body')
     {{-- You can add `mode-dark` class to enable dark mode --}}
-    <div class="bg-gray-100 dark:bg-gray-900 font-sans">
+    <div class="text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-900 font-sans">
         {{ $slot }}
+        dad
     </div>
+
+    @stack('scriptsBefore')
+    <script src="{{ asset('assets/app.js') }}"></script>
+    @stack('scriptsAfter')
 @endsection
